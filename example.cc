da4969d56483411950093689cc717ae3af71382f
Felix Gündling <felix.guendling@gmail.com>
2019-10-21 16:44:05 +0200

    add apple support

--- example.cc
+++ example.cc
@@ -4,5 +4,6 @@
 
 int main() {
   auto r = mysrc::get_resource("example.cc");
+  printf("%zu %p\n", r.size_, r.ptr_);
   printf("%.*s\n", static_cast<int>(r.size_), static_cast<char const*>(r.ptr_));
 }
